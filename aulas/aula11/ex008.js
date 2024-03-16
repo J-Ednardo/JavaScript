a2d1cc79fe9e4df1cee7bfb9c937ee6722d51824
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2024-03-16 14:41:42 -0300

    aula11

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro eh ${velocidade}KM/h`)
+if(velocidade > 60){ //Condição Simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('Digija sempre usando cinto de seguranca')
